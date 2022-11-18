0c84acc34edc39e4a1ab89f0f12b7348cecaa77d
chet <shungen.chen@funplus.com>
2022-11-18 19:01:08 +0800

    鼠标点击移动

--- 3drpg/Assets/Scripts/MouseManager.cs
+++ 3drpg/Assets/Scripts/MouseManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventVecto3 : UnityEvent<Vector3>{}
 
 public class MouseManager : MonoBehaviour
 {
+    private RaycastHit hitInfo;
+
+    public EventVecto3 OnMouseClicked;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,29 @@ public class MouseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SetCursorTexture();
         
+        MouseControl();
+    }
+
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            
+        }
+    }
+
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground"))
+            {
+                OnMouseClicked?.Invoke(hitInfo.point);
+            }
+        }
     }
 }
